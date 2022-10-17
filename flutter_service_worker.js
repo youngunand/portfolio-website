5180af41f9181fc0b0fc9acd94bd4b3d9b61ed7e
Andreas Herzinger <andreas.herzinger02@gmail.com>
2022-10-17 10:37:24 +0200

    uploaded new output

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,61 +3,64 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/assets/dart.png": "3bedf36e2025a39f4c6d74b44d89ae47",
-"assets/assets/github.png": "472739dfb5857b1f659f4c4c6b4568d0",
-"assets/assets/math_mini_game.png": "168cf47881e6944a51d93de4e3c6ef1b",
-"assets/assets/flutter.png": "e26be131ba0a55da6b461a3d36750572",
-"assets/assets/fish_finder_5.png": "39ea08ee8066e71b02ede9e5a2381b47",
-"assets/assets/fishing_exam_2.png": "2cfc26e44a35ea7334e09e20a066f302",
-"assets/assets/profile_picture.jpeg": "14fcc08368a69b31b7044c553e41fadb",
-"assets/assets/raspy_radio.png": "f7ee02ec10490a5e13e80db61dbf15c7",
-"assets/assets/java.png": "0684bf15783118f9adaa52a84a7fce1c",
-"assets/assets/fish_finder_4.png": "c1f361d4ed310ddc7c9a866e71e9764c",
-"assets/assets/rasperry_pi.png": "6382bd58f19d31efa68a9fcd4388bae6",
-"assets/assets/fonts/Dosis-VariableFont_wght.ttf": "ab8cfacc57bad648fadac5f0ff0e39de",
-"assets/assets/fonts/ConcertOne-Regular.ttf": "1b40f0186d1a8b1500b4676b2e9a7ba1",
-"assets/assets/fonts/Overpass-VariableFont_wght.ttf": "676ed13694f673c4bc372d7e4b8ed7fd",
-"assets/assets/fish_finder_1.png": "b0a0fc600f813ba2216971bf07771b94",
-"assets/assets/git.png": "593df29f7efd8dc56c1fb17e0baa1c1b",
-"assets/assets/fishing_exam_1.png": "97756a9f61104c07b051cb9926811537",
-"assets/assets/google_play.png": "a3177372eb221da22db18590a7a93d0b",
-"assets/assets/fishing_exam_4.png": "472ab3b52a6aae41e83f9145cd23a572",
-"assets/assets/math_mini_game_1.png": "f5c99409a6664cf792f1953d1dcf57b7",
-"assets/assets/fishing_exam_5.png": "eb93fb73583dae6377761c775389600f",
-"assets/assets/fishing_exam_3.png": "eb343ec0a7363cd2dfb350f249f27539",
-"assets/assets/python.png": "dbe34c3ef32fa5419ffea560948b9149",
-"assets/assets/fish_finder_3.png": "0952c55b3c1d4cfb320046b5e54bf722",
-"assets/assets/zivi_counter.png": "8ec795585415f9d9825ff46af87150d8",
-"assets/assets/firebase.png": "a3d73b296e3e92b0893abdb7bc7a1868",
-"assets/assets/math_mini_game_2.png": "4d89651e71efa77403acb4ef759ae9ac",
-"assets/assets/fish_finder_2.png": "78901ff6db15954fe8dc0968bfc7c16f",
-"assets/assets/programming_computer.gif": "f11de19df68d2ef24faa4a47fbb578dc",
-"assets/FontManifest.json": "43bd16e570cc7caab276452c4a6e5d25",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "4ff0f6c59de3f4cdef9d5b5068ae61fe",
-"assets/AssetManifest.json": "d5d47c4c026efba6ec0d1d879e929d74",
-"manifest.json": "712de6fd843d0a22566036905b83e212",
-"canvaskit/canvaskit.js": "62b9906717d7215a6ff4cc24efbd1b5c",
-"canvaskit/canvaskit.wasm": "b179ba02b7a9f61ebc108f82c5a1ecdb",
-"canvaskit/profiling/canvaskit.js": "3783918f48ef691e230156c251169480",
-"canvaskit/profiling/canvaskit.wasm": "6d1b0fc1ec88c3110db88caa3393c580",
-"version.json": "94c396ef1fb46b1883ca1bdcd87cc1dd",
-"index.html": "279c4b41bf8516475eba0caf6b0bf5cc",
-"/": "279c4b41bf8516475eba0caf6b0bf5cc",
+  "version.json": "94c396ef1fb46b1883ca1bdcd87cc1dd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"assets/AssetManifest.json": "e7650ebe14b145d1c8361da0002db3f6",
+"assets/NOTICES": "3a9e5998eb1970aaaed7d4f3496c4984",
+"assets/assets/math_mini_game_2.png": "4d89651e71efa77403acb4ef759ae9ac",
+"assets/assets/java.png": "0684bf15783118f9adaa52a84a7fce1c",
+"assets/assets/fish_finder_5.png": "39ea08ee8066e71b02ede9e5a2381b47",
+"assets/assets/profile_picture.jpeg": "14fcc08368a69b31b7044c553e41fadb",
+"assets/assets/math_mini_game.png": "168cf47881e6944a51d93de4e3c6ef1b",
+"assets/assets/programming_computer.gif": "f11de19df68d2ef24faa4a47fbb578dc",
+"assets/assets/fishing_exam_3.png": "eb343ec0a7363cd2dfb350f249f27539",
+"assets/assets/dart.png": "3bedf36e2025a39f4c6d74b44d89ae47",
+"assets/assets/fish_finder_4.png": "c1f361d4ed310ddc7c9a866e71e9764c",
+"assets/assets/fishing_exam_5.png": "eb93fb73583dae6377761c775389600f",
+"assets/assets/git.png": "593df29f7efd8dc56c1fb17e0baa1c1b",
+"assets/assets/fish_finder_1.png": "b0a0fc600f813ba2216971bf07771b94",
+"assets/assets/fish_finder_2.png": "78901ff6db15954fe8dc0968bfc7c16f",
+"assets/assets/python.png": "dbe34c3ef32fa5419ffea560948b9149",
+"assets/assets/fonts/ConcertOne-Regular.ttf": "1b40f0186d1a8b1500b4676b2e9a7ba1",
+"assets/assets/fonts/Overpass-VariableFont_wght.ttf": "676ed13694f673c4bc372d7e4b8ed7fd",
+"assets/assets/fonts/Dosis-VariableFont_wght.ttf": "ab8cfacc57bad648fadac5f0ff0e39de",
+"assets/assets/google_play.png": "a3177372eb221da22db18590a7a93d0b",
+"assets/assets/loading_circle.gif": "c4a7ba862d5a4fec87db9bdfcf3ace7a",
+"assets/assets/github.png": "472739dfb5857b1f659f4c4c6b4568d0",
+"assets/assets/flutter.png": "e26be131ba0a55da6b461a3d36750572",
+"assets/assets/rasperry_pi.png": "6382bd58f19d31efa68a9fcd4388bae6",
+"assets/assets/fishing_exam_2.png": "2cfc26e44a35ea7334e09e20a066f302",
+"assets/assets/roulette_strategies.png": "0c8b760e89aeb9fc656c70091729d203",
+"assets/assets/zivi_counter.png": "8ec795585415f9d9825ff46af87150d8",
+"assets/assets/jupyter.png": "537e9388a94031978b46edf4811a0796",
+"assets/assets/math_mini_game_1.png": "f5c99409a6664cf792f1953d1dcf57b7",
+"assets/assets/fishing_exam_4.png": "472ab3b52a6aae41e83f9145cd23a572",
+"assets/assets/raspy_radio.png": "f7ee02ec10490a5e13e80db61dbf15c7",
+"assets/assets/firebase.png": "a3d73b296e3e92b0893abdb7bc7a1868",
+"assets/assets/fishing_exam_1.png": "97756a9f61104c07b051cb9926811537",
+"assets/assets/fish_finder_3.png": "0952c55b3c1d4cfb320046b5e54bf722",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"assets/shaders/ink_sparkle.frag": "1ecb2904213317e066523eac6b951ad2",
+"assets/FontManifest.json": "43bd16e570cc7caab276452c4a6e5d25",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"manifest.json": "712de6fd843d0a22566036905b83e212",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "4e067d59fbbea8e80465398ab9390b1b"
+"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
+"index.html": "b401ea61f037979d2f245f11fdbe8f0d",
+"/": "b401ea61f037979d2f245f11fdbe8f0d",
+"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
+"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
+"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
+"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
+"main.dart.js": "bbee8c17407558bf2707f6a26dbc9980"
 };
 
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/",
-"main.dart.js",
+  "main.dart.js",
 "index.html",
-"assets/NOTICES",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
 // During install, the TEMP cache is populated with the application shell files.
@@ -156,9 +159,11 @@ self.addEventListener("fetch", (event) => {
     .then((cache) =>  {
       return cache.match(event.request).then((response) => {
         // Either respond with the cached resource, or perform a fetch and
-        // lazily populate the cache.
+        // lazily populate the cache only if the resource was successfully fetched.
         return response || fetch(event.request).then((response) => {
-          cache.put(event.request, response.clone());
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
           return response;
         });
       })
